0814ad644a777d7b2fb3cdd4c2af2d9ac4a939d1
Māris Sinelis <marissi@tvnet.lv>
2022-04-12 21:43:23 +0300

    klases test aplikaciaj

---
+++ Primitivo datu tipu demo/Primitivo datu tipu demo/Program.cs
@@ -0,0 +1,73 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//String myGreeting = "hello my name is Māris";
+//int age = 40;
+//double money = 1450.36;
+//char favoriteSimbol = '@';
+//bool isMale = true;
+
+//Console.WriteLine(myGreeting);
+//Console.WriteLine(age);
+//Console.WriteLine(money);
+//Console.WriteLine(isMale);
+//Console.WriteLine();
+
+//int a = 10;
+//int b = 20;
+
+//int divisionLeft = a % b;
+//Console.WriteLine(divisionLeft);
+
+
+//a++;
+//b--;
+//Console.WriteLine("::",a,b);
+
+//Console.WriteLine();
+//Console.WriteLine("****************");
+//Console.WriteLine();
+
+//int num1 = 10;
+//int num2 = 15;
+//bool isEqual = num1 != num2;
+//Console.WriteLine(isEqual);
+
+
+//Console.WriteLine();
+//Console.WriteLine("****************");
+//Console.WriteLine();
+
+//bool isTrue = true;
+//bool isFalse = false;
+//bool andAction = isTrue && isFalse;
+//Console.WriteLine(andAction);
+
+//Console.WriteLine();
+//Console.WriteLine("****************");
+//Console.WriteLine();
+
+//// Paprasīt lietotājam ievadīt vārdu
+//Console.Write("Ieklabini vārdu? ");
+////Sagaidīt lietotāja ievadi
+//string userName = Console.ReadLine();
+////Sveiciens lietotājam izmantojot vina vārdu
+//Console.WriteLine("Sveiks " + userName + "!");
+
+
+//Console.Write("Ieklabini vecumu? ");
+//string userNumberString = Console.ReadLine();
+//int userNumber = int.Parse(userNumberString);
+//int byTenLarger = userNumber + 10;
+//Console.WriteLine("Par 10 lielaks skaitlis " + byTenLarger);
+
+//int max = 15;
+//int min = 3;
+//int smallernumber = Math.Min(max, min);
+//Console.WriteLine(smallernumber);  
+
+//double noapalojamais = 5.84276;
+//double noapalots = Math.Round(noapalojamais);
+//Console.WriteLine(noapalots);   
+
+string name = "maris";
+string formattedMessage = $"hello {name} it's nice to see u!";
+Console.WriteLine(formattedMessage);
